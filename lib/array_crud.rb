2a6e966631cddaa4e088a050ea03a4cb1b8e3074
Zachary Durr <zdurr22@gmail.com>
2020-09-30 20:15:25 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -12,7 +12,8 @@ array << element
 end
 
 def add_element_to_start_of_array(array, element)
-
+  array = ["item1", "item2"]
+  array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
